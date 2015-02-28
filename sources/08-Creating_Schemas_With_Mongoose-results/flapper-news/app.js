d7cab4f1dc6bfbffbb3ccb1e0dcbb7d10f3b16ee
quartelh <herbert@quartel.net>
2015-02-28 17:40:31 -0500

    Beginning Node done, Mongoose Schemas done

--- sources/08-Creating_Schemas_With_Mongoose-results/flapper-news/app.js
+++ sources/08-Creating_Schemas_With_Mongoose-results/flapper-news/app.js
@@ -10,6 +10,12 @@ var users = require('./routes/users');
 
 var app = express();
 
+var mongoose = require('mongoose');
+require('./models/Posts');
+require('./models/Comments');
+
+mongoose.connect('mongodb://localhost/news')
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
---
+++ sources/08-Creating_Schemas_With_Mongoose-results/flapper-news/models/Posts.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose');
+
+var PostSchema = new mongoose.Schema({
+  title: String,
+  link: String,
+  upvotes: {type: Number, default: 0},
+  comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }]
+});
+
+mongoose.model('Post', PostSchema);
